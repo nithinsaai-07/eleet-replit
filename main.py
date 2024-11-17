c4129836f2974c0012d23073835bca9dc6a5a3bb
Nithin Saai Chengalva <nithinsaai2007@gmail.com>
2024-11-17 05:00:41 +0000

    io setup done

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("hello")
+import sys
+sys.stdin = open("./io/in.txt", "r")
+sys.stdout = open("./io/out.txt", "wt")
+def sqr(x):
+  return(x**2)
+
+def main():
+  t=int(input())
+  for _ in range(t):
+    x=int(input())
+    print(sqr(x))
+main()
